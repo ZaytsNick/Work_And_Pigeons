a5eff576e7838940b7f79a749326ad297facabdd
ZaytsLinux <10kcin01@gmail.com>
2025-01-07 16:29:25 +0500

    local connection of the boost library

--- srv_work_and_pigeons/main.cpp
+++ srv_work_and_pigeons/main.cpp
@@ -1,7 +1,31 @@
+#include <boost/asio.hpp>
 #include <iostream>
-#include <map>
 
-int main()
-{
-    std::cout<< "Hello world!"<<std::endl;
+using namespace boost::asio;
+using ip::tcp;
+
+void start_server(int port) {
+    io_context io_context;
+
+    // Создаем TCP-акцептор на указанном порту
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), port));
+    std::cout << "Server is listening on port " << port << std::endl;
+
+    while (true) {
+        tcp::socket socket(io_context);
+        acceptor.accept(socket);
+
+        std::string message = "Hello, Client!";
+        boost::system::error_code error;
+        write(socket, buffer(message), error);
+        
+        if (!error) {
+            std::cout << "Message sent to client: " << message << std::endl;
+        }
+    }
+}
+
+int main() {
+    start_server(8080);
+    return 0;
 }
